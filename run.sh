19d55ec026fa2fec7d23c2105d304899703537fc
Andrea Manzi <manzolo@libero.it>
2026-04-26 10:03:33 +0200

    feat: add nano, vim, zsh with agnoster theme

--- run.sh
+++ run.sh
@@ -107,6 +107,10 @@ packages:
   - php-curl
   - phpmyadmin
   - curl
+  - zsh
+  - vim
+  - nano
+  - fonts-powerline
 write_files:
   - path: /etc/profile.d/cloud-init-status.sh
     permissions: '0755'
@@ -193,6 +197,13 @@ write_files:
           (2, 'Mouse', 29.99),
           (1, 'Keyboard', 79.99),
           (3, 'Monitor', 349.99);
+  - path: /tmp/setup-zsh.sh
+    permissions: '0755'
+    content: |
+      #!/bin/bash
+      RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+      sed -i 's/^ZSH_THEME=.*/ZSH_THEME="agnoster"/' ~/.zshrc
+      sed -i 's/^plugins=(.*)/plugins=(git)/' ~/.zshrc
 runcmd:
   - chmod -x /etc/update-motd.d/*
   - sed -i 's/^#\?PrintMotd.*/PrintMotd yes/' /etc/ssh/sshd_config
@@ -215,6 +226,8 @@ runcmd:
   - phpenmod mbstring
   - systemctl restart apache2
   - chown -R labuser:labuser /home/labuser
+  - sudo -Hu labuser bash /tmp/setup-zsh.sh
+  - chsh -s /usr/bin/zsh labuser
   - echo "=== mysql-lab VM is ready! ==="
 USERDATA
 
